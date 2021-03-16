1d64b7f8c4a503b0b85e663fe2c035a170c38337
Sergio Aliceral <sergio.aliceral@sesicni.com.br>
2021-03-16 06:56:16 -0300

    first commit

---
+++ src/test/java/br/com/aliceraltecnologia/HelloSpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aliceraltecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
